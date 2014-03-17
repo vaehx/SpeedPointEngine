3f455088febddc1e6b371ddd08b5644afc61ed7c
vaehx <prosicraft@googlemail.com>
2014-03-17 19:32:49 +0100

    Fix minor coding mistakes

--- Include/SMatrix.h
+++ Include/SMatrix.h
@@ -88,13 +88,13 @@ namespace SpeedPoint
 				);
 		}
 
-		SMatrix& operator * (const SVector4& v)
+		SVector4& operator * (const SVector4& v)
 		{
 			return SVector4(
 				m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3] * v.w,
 				m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3] * v.w,
 				m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3] * v.w,
-				m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3] * v.w,
+				m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3] * v.w
 				);
 		}
 
