b7901645b887ef31b57943720c4ea5586fd933cc
vaehx <prosicraft@googlemail.com>
2014-03-21 17:33:48 +0100

    Extend DirectX9Utilites by reverse conversion betw Matrix

--- Include/Implementation/DirectX9/SDirectX9Utilities.h
+++ Include/Implementation/DirectX9/SDirectX9Utilities.h
@@ -11,6 +11,7 @@
 
 namespace SpeedPoint
 {
+	// Convert DirectX Matrix to SpeedPoint Matrix
 	inline SMatrix DXMatrixToSMatrix(const D3DXMATRIX& mat)
 	{
 		return SMatrix(
@@ -18,6 +19,17 @@ namespace SpeedPoint
 			SVector4(mat._21, mat._22, mat._23, mat._24),
 			SVector4(mat._31, mat._32, mat._33, mat._34),
 			SVector4(mat._41, mat._42, mat._43, mat._44)
+			);
+	}
+
+	// Convert SpeedPoint Matrix to DirectX Matrix
+	inline D3DXMATRIX SMatrixToDXMatrix(const SMatrix& mat)
+	{
+		return D3DXMATRIX(
+			mat._11, mat._12, mat._13, mat._14,
+			mat._21, mat._22, mat._23, mat._24,
+			mat._31, mat._32, mat._33, mat._34,
+			mat._41, mat._42, mat._43, mat._44
 			);
 	}
 }
