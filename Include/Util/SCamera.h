f2a2890ae90bcb8003f863a0b531549ad51ad56e
vaehx <prosicraft@googlemail.com>
2014-05-07 14:26:42 +0200

    Add methods to get and calc camera view mtx

--- Include/Util/SCamera.h
+++ Include/Util/SCamera.h
@@ -14,18 +14,34 @@ namespace SpeedPoint
 	class S_API SCamera : public STransformable
 	{
 	private:
-		f32	m_fViewRadius;		// Keep it short for performant rendering
+		f32		m_fViewRadius;		// Keep it short for performant rendering
+		SVector3	m_vLookAt;
+		SMatrix4	m_ViewMatrix;
+		bool		m_bViewMatrixCalculated;	// has the view matrix ever been calculated?
 
 	public:		
 		// Default constructor
 		SCamera()
 			: STransformable(),
-			m_fViewRadius(20.0f)
+			m_fViewRadius(20.0f),
+			m_bViewMatrixCalculated(false)
 		{
 		}
 
+		~SCamera()			
+		{
+			m_bViewMatrixCalculated = false;
+		}
+
 		// Get a view matrix from this camera
-		SMatrix4 GetViewMatrix();
+		SMatrix4& GetViewMatrix();
+
+		// Summary:
+		//	Recalculate the view matrix
+		//	should only be called by the camera itself, but is public to be more flexible, if you want to do it anways...
+		// Return Value:
+		//	Returns new view matrix as const ref
+		SMatrix4& RecalculateViewMatrix();
 
 		// Get view radius of the camera
 		f32 GetViewRadius() { return m_fViewRadius; }
--- Source/Util/SCamera.cpp
+++ Source/Util/SCamera.cpp
@@ -10,11 +10,50 @@ namespace SpeedPoint
 {	
 	// ********************************************************************************************
 
-	S_API SMatrix4 SCamera::GetViewMatrix()
+	S_API inline SMatrix4& SCamera::GetViewMatrix()
 	{
-//~~~~~~~~~~~~~~~~~~
-// TODO !!!!!!!
-//~~~~~~~~~~~~~~~~~~
-		return SMatrix4();
+		if (!m_bViewMatrixCalculated)
+			RecalculateViewMatrix();
+
+		return m_ViewMatrix;
+	}
+
+	// ********************************************************************************************
+
+	S_API inline SMatrix4& SCamera::RecalculateViewMatrix()
+	{
+		//zaxis = normal(Eye - At)
+		//	xaxis = normal(cross(Up, zaxis))
+		//	yaxis = cross(zaxis, xaxis)
+		//
+		//	xaxis.x           yaxis.x           zaxis.x          0
+		//	xaxis.y           yaxis.y           zaxis.y          0
+		//	xaxis.z           yaxis.z           zaxis.z          0
+		//	dot(xaxis, eye)   dot(yaxis, eye)   dot(zaxis, eye)  1
+
+		//D3DXVECTOR3 vEyePt(camera->vPosition.x, camera->vPosition.y, camera->vPosition.z);		
+
+		// !! We do not recalculate the look-at position in each frame, as trigonometrical functions have a high perf cost
+		/*D3DXVECTOR3 vLookAt(
+			camera->vPosition.x + sin(camera->vRotation.y)*cos(camera->vRotation.x),
+			camera->vPosition.y + sin(camera->vRotation.x),
+			camera->vPosition.z + cos(camera->vRotation.y) * cos(camera->vRotation.x)
+			);*/
+
+		// we assume that EyePos - At vector has length 1, as we use unit polar coords
+		SVector3 zaxis = vPosition - m_vLookAt;
+		SVector3 xaxis = SVector3Cross(SVector3(0.0f, 1.0f, 0.0), zaxis).Normalize();
+		SVector3 yaxis = SVector3Cross(zaxis, xaxis);
+
+		m_ViewMatrix = SMatrix4(
+			SVector4(xaxis.x, yaxis.x, zaxis.x, 0),
+			SVector4(xaxis.y, yaxis.y, zaxis.y, 0),
+			SVector4(xaxis.z, yaxis.z, zaxis.z, 0),
+			SVector4(SVector3Dot(xaxis, vPosition), SVector3Dot(yaxis, vPosition), SVector3Dot(zaxis, vPosition), 1.0f)
+			);
+
+		m_bViewMatrixCalculated = true;
+
+		return m_ViewMatrix;
 	}
 }
