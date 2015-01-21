81721a9c3128bb22ba010bcee05d6bfc462c81cf
vaehx <prosicraft@googlemail.com>
2015-01-21 10:38:14 +0100

    Implement terrain low-poly generation

--- Include/Abstract/ITerrain.h
+++ Include/Abstract/ITerrain.h
@@ -19,6 +19,36 @@ struct S_API ITexture;
 struct S_API IVertexBuffer;
 struct S_API IIndexBuffer;
 
+struct S_API STerrainDescription
+{
+	unsigned int nX, nZ;
+	unsigned int nChunkSideSegments;
+	float width, depth;
+	float baseHeight;
+	bool bDynamic;
+
+	STerrainDescription()		
+		: bDynamic(false)
+	{
+	}
+
+	STerrainDescription(const STerrainDescription& tdsc)
+		: nX(tdsc.nX),	
+		nZ(tdsc.nZ),
+		nChunkSideSegments(tdsc.nChunkSideSegments),
+		width(tdsc.width),
+		depth(tdsc.depth),
+		baseHeight(tdsc.baseHeight)
+	{
+	}
+
+	inline bool IsValid() const
+	{
+		return (nX > 0 && nZ > 0 && nChunkSideSegments > 0 && width > 0 && depth > 0);
+	}
+};
+
+
 // SpeedPoint Terrain Solid (abstract)
 struct S_API ITerrain
 {
@@ -29,10 +59,10 @@ public:
 
 	// Initialize with the engine
 	// nX and nZ is the resolution
-	virtual SResult Initialize( IGameEngine* pEngine, unsigned int nX, unsigned int nZ ) = 0;
+	virtual SResult Initialize(IGameEngine* pEngine) = 0;
 
-	// Create a planar terrain with Size fW x fD and resolution nX, nY	
-	virtual SResult CreatePlanar( float fW, float fD, float baseHeight, bool bDynamic = false ) = 0;
+	// Create a planar terrain using given terrain description	
+	virtual SResult CreatePlanar(const STerrainDescription& tdsc) = 0;
 
 	virtual SResult RecalculateNormals() = 0;
 
@@ -40,7 +70,7 @@ public:
 	virtual SResult SetDetailMap(ITexture* pDetailMap) = 0;
 
 	virtual IVertexBuffer* GetVertexBuffer() = 0;
-	virtual IIndexBuffer* GetIndexBuffer() = 0;
+	virtual IIndexBuffer* GetIndexBuffer(unsigned int lodLevel) = 0;
 	
 	// Summary:
 	//	Flags that the terrain Constant Buffer has to be updated.
@@ -56,7 +86,7 @@ public:
 	///////////////////
 
 	// Render the Terrain. Fails if pointer to SpeedPointEngine is not correct
-	virtual SResult RenderTerrain( void ) = 0;
+	virtual SResult RenderTerrain( const SVector3& lodCenterPos ) = 0;
 
 	// Clearout the terrain data BUT not the heightmap
 	virtual SResult Clear( void ) = 0;
