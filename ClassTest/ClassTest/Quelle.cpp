bd5bddd84c00469758020ca84ef98756b2d35766
vaehx <prosicraft@googlemail.com>
2015-01-30 11:31:30 +0100

    Delete unnecessary ClassTest Proj files + finiish terrainlodgen

--- ClassTest/ClassTest/Quelle.cpp
+++ ClassTest/ClassTest/Quelle.cpp
@@ -1,10 +1,77 @@
 #include <stdio.h>
 
+
+unsigned int CountSideParts(unsigned int w, unsigned int startsz)
+{
+	unsigned int sz = startsz;
+	unsigned int accumulator = 0;
+	while (w > 0)
+	{
+		while ((int)w - (int)sz < 0)
+			sz = (sz > 1) ? (unsigned int)((double)sz * 0.5) : 0;
+
+		if (sz == 0)
+			break;
+
+		accumulator++;
+		w -= sz;
+	}
+
+	return accumulator;
+}
+
 void main()
 {
+	unsigned int startsz = 4;
 
-	
+
+	unsigned int w = 0, d = 8;
+	unsigned int sz = startsz;
+
+	unsigned int xacc = CountSideParts(w, startsz);
+	unsigned int zacc = CountSideParts(d, startsz);
+
+	printf("========================================\n");
+	unsigned int nIndices = (xacc * zacc) * 6;
+	unsigned int nVertices = (w * d > 0) ? (xacc + 1) * (zacc + 1) : 0;
+	printf("Need generate %dx%d (%d) quads, which means %d indices (6 per quad) and %d vertices\n", xacc, zacc, xacc * zacc, nIndices, nVertices);
+
+
+
+
 
 	// ---------------
 	getchar();
 }
+
+
+
+/*
+
+
+o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o
+|                                               |                                               |
+|                                               |                                               |
+o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o
+|                                               |                                               |
+|                                               |                                               |
+o                                               o                                               o
+|                                               |                                               |
+|                                               |                                               |
+o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o
+|                                               |                                               |
+|                                               |                                               |
+o                                               o                                               o
+|                                               |                                               |
+|                                               |                                               |
+o                                               o                                               o
+|                                               |                                               |
+|                                               |                                               |
+o                                               o                                               o
+|                                               |                                               |
+|                                               |                                               |
+o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o-----o
+
+
+
+*/
--- TerrainLodGeneration.c
+++ TerrainLodGeneration.c
@@ -84,7 +84,7 @@ struct STerrain
     float fWidth, fDepth;
     
     STerrainChunk* pTerrainChunks;
-    unsigned int nChunksX, nChunksZ;
+    unsigned int nChunksX, nChunksZ; // including rest chunks
     unsigned int nRestChunksX, nRestChunksZ; // essentially 0 or 1    
     
     // Top rest chunk: (nChunkSegsX, nZRestChunkSegs)
@@ -131,43 +131,38 @@ void STerrain::Generate(unsigned int segsX, unsigned int segsZ,
         nRestChunksZ = 1;
     }
     
+    // calculate segment sizes
+    float segWidth = w / nSegsX;
+    float segDepth = d / nSegsZ;
+    float segUSz = segWidth;
+    float segVSz = segDepth;        
+    
     // generate chunks buffer
     pTerrainChunks = new STerrainChunk[nChunksX * nChunksZ];
         
     // Precalculate vertex and index count, create the buffers and create chunks
-    pLodLevelCounts = new unsigned int[nLodLevels][2];
+    pLodLevelCounts = new STerrainChunkCounts[nLodLevels];
     pVertexBuffers = new SVertex*[nLodLevels];
     pIndexBuffers = new SIndex*[nLodLevels];
     for (unsigned int lodLvl = 0; lodLvl < nLodLevels; ++lodLvl)
-    {        
-        unsigned int &vtxCnt = pLodLevelCounts[lodLvl][0];
-        unsigned int &idxCnt = pLodLevelCounts[lodLvl][1];
-        vtxCnt = 0;
-        idxCnt = 0;                
-        unsigned int regChunkVtxCnt = STerrainChunk::PrecalcVertexCount(lodLvl, nChunkSegsX, nChunkSegsZ);
-        unsigned int regChunkIdxCnt = STerrainChunk::PrecalcIndexCount(lodLvl, nChunkSegsX, nChunkSegsZ);
+    {   
+        STerrainChunkCounts regChunkCounts = STerrainChunk::PrecalcCounts(lodLvl, nChunkSegsX, nChunkSegsZ);
+        STerrainChunkCounts restXChunkCnt = STerrainChunk::PrecalcCounts(lodLvl, nXRestChunkSegs, nChunkSegsZ);
+        STerrainChunkCounts restZChunkCnt = STerrainChunk::PrecalcCounts(lodLvl, nChunkSegsX, nZRestChunkSegs);
+        STerrainChunkCounts fillChunkCnt = STerrainChunk::PrecalcCounts(lodLvl, nXRestChunkSegs, nZRestChunkSegs);
+                                  
+        vtxCnt = regChunkCounts.vertices * (nChunksX - nRestChunksX) * (nChunksZ - nRestChunksZ);
+        idxCnt = regChunkCounts.indices * (nChunksX - nRestChunksX) * (nChunksZ - nRestChunksZ);
         
-        vtxCnt += regChunkVtxCnt * (nChunksX - nRestChunksX) * (nChunksZ - nRestChunksZ);
-        idxCnt += regChunkIdxCnt * (nChunksX - nRestChunksX) * (nChunksZ - nRestChunksZ);
-
-
-        unsigned int restXChunkCnt = STerrainChunk::PrecalcVertexCount(lodLvl, nXRestChunkSegs, nChunkSegsZ);
-        unsigned int restZChunkCnt = STerrainChunk::PrecalcVertexCount(lodLvl, nChunkSegsX, nZRestChunkSegs);
-        unsigned int fillChunkCnt = STerrainChunk::PrecalcVertexCount(lodLvl, nXRestChunkSegs, nZRestChunkSegs);
-        
-        unsigned int restXChunkCnt = STerrainChunk::PrecalcIndexCount(lodLvl, nXRestChunkSegs, nChunkSegsZ);
-        unsigned int restZChunkCnt = STerrainChunk::PrecalcIndexCount(lodLvl, nChunkSegsX, nZRestChunkSegs);
-        unsigned int fillChunkCnt = STerrainChunk::PrecalcIndexCount(lodLvl, nXRestChunkSegs, nZRestChunkSegs);
-
         if (nRestChunksX > 0 || nRestChunksZ > 0)
         {
-            vtxCnt += nRestChunksX * (nChunksZ - nRestChunksZ) * restXChunkCnt;                               
-            vtxCnt += nRestChunksZ * (nChunksX - nRestChunksX) * restZChunkCnt;
-            vtxCnt += nRestChunksX * nRestChunksZ * fillChunkCnt;                                                            
+            vtxCnt += nRestChunksX * (nChunksZ - nRestChunksZ) * restXChunkCnt.vertices;                               
+            vtxCnt += nRestChunksZ * (nChunksX - nRestChunksX) * restZChunkCnt.vertices;
+            vtxCnt += nRestChunksX * nRestChunksZ * fillChunkCnt.vertices;                                                            
         
-            idxCnt += nRestChunksX * (nChunksZ - nRestChunksZ) * restXChunkCnt;                               
-            idxCnt += nRestChunksZ * (nChunksX - nRestChunksX) * restZChunkCnt;
-            idxCnt += nRestChunksX * nRestChunksZ * fillChunkCnt;    
+            idxCnt += nRestChunksX * (nChunksZ - nRestChunksZ) * restXChunkCnt.indices;                               
+            idxCnt += nRestChunksZ * (nChunksX - nRestChunksX) * restZChunkCnt.indices;
+            idxCnt += nRestChunksX * nRestChunksZ * fillChunkCnt.vertices;    
         }
         
         pVertexBuffers[lodLvl] = new SVertex[vtxCnt];
@@ -185,83 +180,178 @@ void STerrain::Generate(unsigned int segsX, unsigned int segsZ,
                 bool bIsXRest = (cx == nChunksX - 1) && (nRestChunksX > 0); 
         
                 STerrainChunk* pChunk = pTerrainChunks[cz * nChunksX + cx];
+                
+                // Initialize chunk
                 if (lodLvl == 0)
                 {
                     pChunk->pVertices = new SIndex*[nLodLevels];
                     pChunk->pIndices = new SIndex*[nLodLevels];
-                    pChunk->nLodLevels = nLodLevels;
+                    pChunk->nLodLevels = nLodLevels;                    
                     
                     pChunk->nSegsX = (bIsXRest) ? nXRestChunkSegs : nChunkSegsX;
-                    pChunk->nSegsZ = (bIsZRest) ? nZRestChunkSegs : nChunkSegsZ;                                                               
+                    pChunk->nSegsZ = (bIsZRest) ? nZRestChunkSegs : nChunkSegsZ;
+                    pChunk->fWidth = nSegsX * segWidth;
+                    pChunk->fDepth = nSegsZ * segDepth;
+                    pChunk->fSegUSz = segUSz;
+                    pChunk->fSegVSz = segVSz;
+                    pChunk->fXOffs = cx * (segWidth * nChunkSegsX);
+                    pChunk->fZOffs = cz * (segDepth * nChunkSegsZ);
+                    pChunk->fUOffs = pChunk->fXOffs;
+                    pChunk->fVOffs = pChunk->fZOffs;                                                               
                 }
                 
                 pChunk->pVertices[lodLevel] = &pVertexBuffers[lodLvl][iCurVtxPos];
                 pChunk->pIndices[lodLevel] = &pIndexBuffers[lodLvl][iCurIdxPos];                                                  
                 
-                pChunk->GenerateLodLevel(lodLvl, (lodLvl == 0) ? 1 : lodLvl * 2);                            
+                pChunk->GenerateLodLevel(lodLvl, STerrainChunk::GetLodLevelQuadStartSz(lodLvl));                            
             }
         }                                                                                                                          
     }                              
 }
 
 
+
+
+
+
+struct STerrainChunkCounts
+{
+    unsigned int indices, vertices;
+    unsigned int quadsX, quadsZ;    
+};
+
 struct STerrainChunk
 {
 	unsigned int nSegsX, nSegsZ; // on high res grid
+    float fWidth, fDepth;
+    float fXOffs, fZOffs;
+    float fUOffs, fVOffs;
+    float fSegUSz, fSegVSz;
     SIndex** pIndices; // (SIndex[nIndices])[nLodLevels]
     SVertex** pVertices; // (SVertex[nIndices])(nLodLevels)
     unsigned int nLodLevels;    
     
-    static unsigned int PrecalcIndexCount(unsigned int lodLevel, unsigned int segsX, unsigned int segsZ);
-    static unsigned int PrecalcVertexCount(unsigned int lodLevel, unsigned int segsX, unsigned int segsZ);
-    void AddQuad(unsigned int lodLevel, unsigned int w, unsigned int d, unsigned int iStartVtx);
-    void GenerateLodLevel(unsigned int lodLevel, unsigned int startSz);
+    static unsigned int CountSideParts(unsigned int w, unsigned int startsz)
+    inline static unsigned int GetLodLevelQuadStartSz(unsigned int lodLevel)
+    {
+        return (lodLevel == 0) ? 1 : lodLvl * 2;
+    }
+        
+    static STerrainChunkCounts PrecalcCounts(unsigned int lodLevel, unsigned int segsX, unsigned int segsZ);
+        
+    void AddQuad(unsigned int lodLevel, unsigned int w, unsigned int d, unsigned int iStartVtx, float baseHeight);
+    
+    // startSz - Size of biggest quad
+    void GenerateLodLevel(unsigned int lodLevel, unsigned int startSz, float baseHeight);
 };
 
 
-static unsigned int STerrainChunk::PrecalcIndexCount(unsigned int lodLevel, unsigned int segsX, unsigned int segsZ)
+static unsigned int CountSideParts(unsigned int l, unsigned int lodLvl)
 {
+	unsigned int sz = GetLodLevelQuadStartSz(lodLvl);
+	unsigned int accumulator = 0;
+	while (l > 0)
+	{
+		while ((int)l - (int)sz < 0)
+			sz = (sz > 1) ? (unsigned int)((double)sz * 0.5) : 0;
+
+		if (sz == 0)
+			break;
+
+		accumulator++;
+		l -= sz;
+	}
+
+	return accumulator;
 }
 
-static unsigned int STerrainChunk::PrecalcVertexCount(unsigned int lodLevel, unsigned int segsX, unsigned int segsZ)
+static STerrainChunkCounts STerrainChunk::PrecalcCounts(unsigned int lodLevel, unsigned int segsX, unsigned int segsZ)
 {
+    STerrainChunkCounts counts;
+    counts.quadsX = CountSideParts(segsX, lodLevel);
+    counts.quadsZ = CountSideParts(segsZ, lodLevel);
+    
+    if (segsX * segsZ > 0)
+    {
+        counts.indices = counts.quadsX * counts.quadsZ;
+        counts.vertices = (counts.quadsX + 1) * (counts.quadsZ + 1);
+    }
+    else
+    {
+        counts.indices = 0;
+        counts.vertices = 0;
+    }
+    
+    return counts;                   
 }
 
 
-void STerrainChunk::AddQuad(unsigned int lodLevel, unsigned int w, unsigned int d, unsigned long iStartVtx)
+void STerrainChunk::AddQuad(unsigned int lodLevel, unsigned int w, unsigned int d,
+    unsigned long iStartVtx, unsigned long iStartIndex, unsigned long vtxRowShift, float baseHeight, float xoffs, float zoffs, float uoffs, float voffs)
 {
-    pIndices[curIdx] = 
+    // generate vertices
+    if (!pVertices[lodLevel] || !pIndics[lodLevel])
+        return; // unexpected critical
+        
+    float quadWidth = fWidth / w;
+    float quadDepth = fDepth / d;
+        
+    float tangent[3] = { -1.0f, 0,0 };
+        
+    pVertices[iStartVtx] = SVertex(xoffs, baseHeight, zoffs, 0, 1.0f, 0, tangent[0], tangent[1], tangent[2], uoffs, voffs);                                
+    pVertices[iStartVtx + 1] = SVertex(xoffs + quadWidth, baseHeight, zoffs, 0, 1.0f, 0, tangent[0], tangent[1], tangent[2], uoffs + fSegUSz, voffs);
+    pVertices[iStartVtx + vtxRowShift] = SVertex(xoffs, baseHeight, zoffs + quadDepth, 0, 1.0f, 0, tangent[0], tangent[1], tangent[2], uoffs, voffs + fSegVSz);
+    pVertices[iStartVtx + vtxRowShift + 1]  = SVertex(xoffs + quadWidth, baseHeight, zoffs + quadDepth, 0, 1.0f, 0, tangent[0], tangent[1], tangent[2], uoffs + fSegUSz, voffs + fSegVSz);
+    
+    pIndices[iStartIndex] = iStartVtx;
+    pIndices[iStartIndex + 1] = iStartVtx + vtxRowShift + 1;
+    pIndices[iStartIndex + 2] = iStartVtx + 1;
+    pIndices[iStartIndex + 3] = iStartVtx;
+    pIndices[iStartIndex + 4] = iStartVtx + vtxRowShift;
+    pIndices[iStartIndex + 5] = iStartVtx + vtxRowShift + 1;    
 }
 
 
 // startSz needs to be odd, or 1
-void STerrainChunk::GenerateLodLevel(unsigned int lodLvl, unsigned int startSz /* start quad size */)
+void STerrainChunk::GenerateLodLevel(unsigned int lodLvl, unsigned int startSz /* start quad size */, float baseHeight)
 {
 	if (startSz % 2 > 0 && startSz > 1)
 		return;
 
 	unsigned int sz[2] = { startSz, startSz };
 	unsigned int w = nSegsX, d = nSegsZ;
+    
+    unsigned int vtxRowShift = nSegsX + 1;
 
-	for (unsigned int z = 0; z < cd;)
+    unsigned long iCurVtx = 0;
+    unsigned long iCurIdx = 0;
+    
+    float quadWidth = fWidth / w;
+    float quadDepth = fDepth / d;
+
+	for (unsigned int z = 0; z < nSegsZ;)
 	{
 		// find next quad depth
-		while (d - sz[1] < 0)
+		while ((int)d - (int)sz[1] < 0)
 			sz[1] = (sz[1] > 1) ? (unsigned int)((double)sz[1] * 0.5) : 0;
             
         if (sz[1] == 0)
             break; // should not happen tho, because z == cd then            
 
-		for (unsigned int x = 0; x < cw;)
+		for (unsigned int x = 0; x < nSegsX;)
 		{
 			// find next quad width
-            while (w - sz[0] < 0)
+            while ((int)w - (int)sz[0] < 0)
                 sz[0] = (sz[0] > 1) ? (unsigned int)((double)sz[0] * 0.5) : 0;
                 
             if (sz[0] == 0)
                 break; // should not happen tho, because x == cw then
+                       
+            AddQuad(lodLvl, sz[0], sz[1], iCurVtx, iCurIdx, vtxRowShift, baseHeight,
+                fXOffs + quadWidth * x, fZOffs + quadDepth * z, fUOffs + quadWidth * x, fVOffs + quadDepth * z);
             
-            addQuad()                            
+            iCurVtx += (x == nSegsX - 1) ? 2 : 1;
+            iCurIdx += 6;                            
             
             w -= sz[0];
             x += sz[0];
